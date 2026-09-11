ef920f40b42749336c06c888ec88cded44121548
Xin898 <37047545+Xin898@users.noreply.github.com>
2026-09-11 23:40:07 +0200

    epic: add src/main/java/com/xin898/trademonitor/TradeMonitorApplication.java

---
+++ src/main/java/com/xin898/trademonitor/TradeMonitorApplication.java
@@ -0,0 +1,11 @@
+package com.xin898.trademonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeMonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TradeMonitorApplication.class, args);
+    }
+}
